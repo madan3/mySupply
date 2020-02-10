491b78ce5fbae421a2b4e4abd7444e29f27e1593
madan adhikari <madanadhikari1986@gmail.com>
2020-02-11 00:46:02 +0100

    initial commit

--- database/seeds/CrietariesTableSeeder.php
+++ database/seeds/CrietariesTableSeeder.php
@@ -13,11 +13,6 @@ class CrietariesTableSeeder extends Seeder
     public function run()
     {
         if(\App::environment('local')) {
-            //factory(\App\Entity\SupplierProduct::class, 1)->create();
-            //factory(Criteria::class, 1)->states('Processor Speed')->create();
-            //factory(Criteria::class, 1)->states('Screen Resolution')->create();
-            //factory(Criteria::class, 1)->states('RAM Amount')->create();
-            //factory(Criteria::class, 1)->states('Energy Star Certificate')->create();
             factory(Criteria::class, 1)->states('Price')->create();
         }
     }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,14 +14,8 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         if(App::environment('local')) {
-            //factory(\App\Entity\User::class, 1)->create();
-            //factory(User::class, 1)->states('Customer')->create();
-            //factory(User::class, 1)->states('Dell Supplier')->create();
-            //factory(User::class, 1)->states('Lenova Supplier')->create();
-            //factory(User::class, 1)->states('Asus Supplier')->create();
+            //Create Customer user type
             factory(User::class, 1)->states('Customer')->create()->each(function($user){
-//                $pSpeed = factory(Criteria::class, 1)->states('Processor Speed')->create();
-//                $pSpeed->users()->attach($user->id);
                 $user->criterias()->attach(factory(Criteria::class, 1)->states('Processor Speed')->create());
                 $user->criterias()->attach(factory(Criteria::class, 1)->states('Screen Resolution')->create());
                 $user->criterias()->attach(factory(Criteria::class, 1)->states('RAM Amount')->create());
